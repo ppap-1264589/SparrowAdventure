9681a1db45b326f36821bc162f1acabafdf468bb
JustABoyWithHisViolin <169814442+tddy314@users.noreply.github.com>
2024-10-02 22:41:38 +0700

    Update HelpMethods.java
    Hoàn thành video #10

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -2,6 +2,9 @@ package utilz;
 
 import main.Game;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
 public class HelpMethods {
 	public static boolean CanMoveHere(float x, float y, float width, float height, int lvlData[][]) { 
 		/*Bắt đầu check xem:
@@ -45,5 +48,59 @@ public class HelpMethods {
 		 */
 		
 		return false;
+	}
+
+	public static float GetEntityXPosNExtToWall(Rectangle2D.Float hitbox, float xSpeed) {
+		// Ham nay se tra ve vi tri cua hitbox.x ke canh voi wall nhat co the
+
+		int currentTile = (int)(hitbox.x / Game.TILES_SIZE);
+		// cot ma nhan vat dang dung
+
+		if(xSpeed > 0) {
+			// RIGHT
+			// Nhan vat dang di ve ben phai
+
+
+			int tileXPos = currentTile * Game.TILES_SIZE; // Vi tri cua nhan vat theo x cua tile hien tai
+			int xOffset = (int)(Game.TILES_SIZE - hitbox.width);
+			// Khoang cach giua canh ben trai cua hitbox voi canh ben trai cua currtenTile
+			// khi hitbox da sat vao canh ben phai cua currentTile;
+			//  |        |
+			//  |_____|  |
+			//  | kc  |  |
+			return tileXPos + xOffset - 1;
+			// Tru 1 de hitbox khong overlap voi wall
+			// tuc la luon ton tai khoang cach dx = 1 pixel giua bien hitbox voi wall
+		}else {
+			// LEFT
+			// Nhan vat dang di ve ben trai
+			return currentTile * Game.TILES_SIZE;
+		}
+
+		//xSpeed != 0 vi neu xSpeed == 0 se khong co collision
+	}
+
+	public static float GetEntityYPosUnderRoofOrAboveFloor(Rectangle2D.Float hitbox, float airSpeed) {
+		//Ham nay tuong tu ham ben tren nhung doi voi y
+
+		int currentTile = (int)(hitbox.y / Game.TILES_SIZE);// hang ma nhan vat dang dung
+
+		if(airSpeed > 0) { // FALLING - TOUCHING FLOOR
+			int tileYPos = currentTile * Game.TILES_SIZE; // Vi tri cua nhan vat theo x cua tile hien tai
+			int yOffset = (int)(Game.TILES_SIZE - hitbox.height);
+			return  tileYPos + yOffset - 1;
+		}else { // JUMPING
+			return currentTile * Game.TILES_SIZE;
+		}
+	}
+
+	public static boolean IsEntityOnFloor(Rectangle2D.Float hitbox, int[][] lvldata) {
+		// Check the pixel below bottom-left and bottom-right
+		if(!IsSolid(hitbox.x, hitbox.y + hitbox.height + 1, lvldata)) {// Kiem tra diem Duoi Trai cua hitbox
+			if(!IsSolid(hitbox.x + hitbox.width, hitbox.y + hitbox.height + 1, lvldata)) {// Kiem tra Duoi phai
+				return false;
+			}
+		}
+		return true;
 	}
 }
