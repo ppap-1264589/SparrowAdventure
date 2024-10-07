438c8dd3432b98bbbdc48b1300058ed57fe5db59
JustABoyWithHisViolin <169814442+tddy314@users.noreply.github.com>
2024-10-07 23:55:25 +0700

    Update Constants.java
    Moving BackGround

--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -1,6 +1,25 @@
 package utilz;
 
+import main.Game;
+
+import java.security.PublicKey;
+
 public class Constants {
+
+	public static class Environment {
+		public static final int BIG_CLOUD_WIDTH_DEFAULT = 448;
+		public static final int BIG_CLOUD_HEIGHT_DEFAULT = 101;
+		public static final int BIG_CLOUD_WIDTH = (int)(BIG_CLOUD_WIDTH_DEFAULT * Game.SCALE);
+		public static final int BIG_CLOUD_HEIGHT = (int)(BIG_CLOUD_HEIGHT_DEFAULT * Game.SCALE);
+
+
+		public static final int SMALL_CLOUD_WIDTH_DEFAULT = 74;
+		public static final int SMALL_CLOUD_HEIGHT_DEFAULT = 24;
+		public static final int SMALL_CLOUD_WIDTH = (int)(SMALL_CLOUD_WIDTH_DEFAULT * Game.SCALE);
+		public static final int SMALL_CLOUD_HEIGHT = (int)(SMALL_CLOUD_HEIGHT_DEFAULT * Game.SCALE);
+
+	}
+
 	public static class Directions{ //cac huong di chuyen cua nhan vat
 		public static final int LEFT = 0;
 		public static final int UP = 1;
