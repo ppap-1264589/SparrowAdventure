38c9e2ef35589dec869a0b2a8de11955e8be9725
JustABoyWithHisViolin <169814442+tddy314@users.noreply.github.com>
2024-10-07 23:27:07 +0700

    Update LoadSave.java
    Bigger Map

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -13,7 +13,7 @@ public class LoadSave {
 
 	public static final String PLAYER_ATLAS = "player_sprites.png";
 	public static final String LEVEL_ATLAS = "outside_sprites.png";
-	public static final String LEVEL_ONE_DATA = "level_one_data.png";
+	public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
@@ -34,8 +34,10 @@ public class LoadSave {
 	}
 	
 	public static int[][] GetLevelData(){
-		int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+
 		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+		int[][] lvlData = new int[img.getHeight()][img.getWidth()];
+
 		
 		for (int j = 0; j < img.getHeight(); j++) {
 			for (int i = 0; i < img.getWidth(); i++) {
