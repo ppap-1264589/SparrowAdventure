c10552f4b18eab22bea6a1d033254ffddc91d698
JustABoyWithHisViolin <169814442+tddy314@users.noreply.github.com>
2024-10-10 00:04:15 +0700

    Update Constants.java
    add enemy

--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -6,6 +6,45 @@ import java.security.PublicKey;
 
 public class Constants {
 
+	public static class EnemyConstants {
+		public static final int CRABBY = 0;
+
+		public static final int IDLE = 0;
+		public static final int RUNNING = 1;
+		public static final int ATTACK = 2;
+		public static final int HIT = 3;
+		public static final int DEAD = 4;
+
+		public static final int CRABBY_WIDTH_DEFAULT = 72;
+		public static final int CRABBY_HEIGHT_DEFAULT = 32;
+
+		public static final int CRABBY_WIDTH = (int)(CRABBY_WIDTH_DEFAULT * Game.SCALE);
+		public static final int CRABBY_HEIGHT = (int)(CRABBY_HEIGHT_DEFAULT * Game.SCALE);
+
+		public static final int CRABBY_DRAWOFFSET_X = (int)(26 * Game.SCALE);
+		public static final int CRABBY_DRAWOFFSET_Y = (int)(9 * Game.SCALE);
+		// khoang cach giua sprite va hitbox theo x, y;
+
+		public static int GetSpriteAmount(int enemyType, int enemyState) {
+			switch (enemyType) {
+				case CRABBY:
+					switch (enemyState) {
+						case IDLE:
+							return 9;
+						case RUNNING:
+							return 6;
+						case ATTACK:
+							return 7;
+						case  HIT:
+							return 4;
+						case DEAD:
+							return 5;
+					}
+			}
+			return 0;
+		}
+	}
+
 	public static class Environment {
 		public static final int BIG_CLOUD_WIDTH_DEFAULT = 448;
 		public static final int BIG_CLOUD_HEIGHT_DEFAULT = 101;
