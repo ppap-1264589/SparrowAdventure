0eff95f26056ca5119586a26a92a9543153ba976
ppap-1264589 <88699088+ppap-1264589@users.noreply.github.com>
2024-10-13 16:42:43 +0700

    Add files via upload

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -20,29 +20,50 @@ public class MouseInputs implements MouseListener, MouseMotionListener{
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		
+		switch (Gamestate.state) {
+		case MENU:
+			gamePanel.getGame().getMenu().mouseMoved(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().mouseMoved(e);
+			break;
+		}
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		switch (Gamestate.state) {
-			case MENU:
-				gamePanel.getGame().getMenu().mouseClicked(e);
-				break;
-			case PLAYING:
-				gamePanel.getGame().getPlaying().mouseClicked(e);
-				break;
+		case MENU:
+			gamePanel.getGame().getMenu().mouseClicked(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().mouseClicked(e);
+			break;
 		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
+		switch (Gamestate.state) {
+		case MENU:
+			gamePanel.getGame().getMenu().mousePressed(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().mousePressed(e);
+			break;
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
+		switch (Gamestate.state) {
+		case MENU:
+			gamePanel.getGame().getMenu().mouseReleased(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().mouseReleased(e);
+			break;
+		}
 	}
 
 	@Override
