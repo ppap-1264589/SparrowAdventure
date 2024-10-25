6eaa62aa47e78961bf446b1ab5f8949203bb2089
ppap-1264589 <doxuanhoangnguyenthienthuat@gmail.com>
2024-10-25 22:47:58 +0700

    Add files via upload

--- src/audio/AudioPlayer.java
+++ src/audio/AudioPlayer.java
@@ -3,7 +3,15 @@ package audio;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
-import javax.sound.sampled.*;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.BooleanControl;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 
 public class AudioPlayer {
 
