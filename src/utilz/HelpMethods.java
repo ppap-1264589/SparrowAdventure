ae672d4a042eb6da8c01bbcac5d5de040cce793c
ppap-1264589 <doxuanhoangnguyenthienthuat@gmail.com>
2024-10-25 22:54:17 +0700

    Add files via upload

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -1,43 +1,60 @@
 package utilz;
 
-import main.Game;
-
-import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
+import main.Game;
+import objects.Projectile;
+
 public class HelpMethods {
-	public static boolean CanMoveHere(float x, float y, float width, float height, int lvlData[][]) { 
+
+	public static boolean CanMoveHere(float x, float y, float width, float height, int[][] lvlData) {
 		/*Bắt đầu check xem:
 		 nhân vật có vị trí (x, y) với độ lớn (của hitbox) là hình chữ nhật w*h, có thể di chuyển
 		 đến một vị trí đã có trong lvlData[][] hay không?
 		*/
-		
-		if (!IsSolid(x, y, lvlData)) {
-			if (!IsSolid(x + width, y + height, lvlData)) {
-				if (!IsSolid(x + width, y, lvlData)) {
-					if (!IsSolid(x, y + height, lvlData)) {
+		if (!IsSolid(x, y, lvlData))
+			if (!IsSolid(x + width, y + height, lvlData))
+				if (!IsSolid(x + width, y, lvlData))
+					if (!IsSolid(x, y + height, lvlData))
 						return true;
-					}
-				}
-			}
-		}
 		return false;
 	}
-	
-	private static boolean IsSolid(float x, float y, int[][] lvlData) {
-		int maxWidth = lvlData[0].length * Game.TILES_SIZE;
-		if (x < 0 || x >= maxWidth) return true;
-		if (y < 0 || y >= Game.GAME_HEIGHT) return true;
-		
-		float xIndex = x/Game.TILES_SIZE;
-		float yIndex = y/Game.TILES_SIZE;
-		//in fact: mỗi tọa độ (x, y) của mình, đều tương đương với một cái tile nào đấy trong game
-		return IsTileSolid((int)xIndex, (int)yIndex, lvlData);
 
+	private static boolean IsSolid(float x, float y, int[][] lvlData) {
+		//Dựa theo lvlData đã được nạp, nhân lên với TILES_SIZE, ta có kích thước của toàn bộ lvl về mặt độ dài
+		int maxWidth = lvlData[0].length * Game.TILES_SIZE; 
+		if (x < 0 || x >= maxWidth)
+			return true;
+		if (y < 0 || y >= Game.GAME_HEIGHT)
+			return true;
+		float xIndex = x / Game.TILES_SIZE;
+		float yIndex = y / Game.TILES_SIZE;
+		//in fact: mỗi tọa độ (x, y) của mình, đều tương đương với một cái tile nào đấy trong game
+
+		return IsTileSolid((int) xIndex, (int) yIndex, lvlData);
 	}
 
-	public static boolean IsTileSolid(int xIndex, int yIndex, int [][] lvlData) {
-		int value = lvlData[(int)yIndex][(int)xIndex];
+	public static boolean IsProjectileHittingLevel(Projectile p, int[][] lvlData) {
+		return IsSolid(p.getHitbox().x + p.getHitbox().width / 2, p.getHitbox().y + p.getHitbox().height / 2, lvlData);
+	}
+
+	public static boolean IsEntityInWater(Rectangle2D.Float hitbox, int[][] lvlData) {
+		// Will only check if entity touch top water. Can't reach bottom water if not
+		// touched top water.
+		if (GetTileValue(hitbox.x, hitbox.y + hitbox.height, lvlData) != 48)
+			if (GetTileValue(hitbox.x + hitbox.width, hitbox.y + hitbox.height, lvlData) != 48)
+				return false;
+		return true;
+	}
+
+	private static int GetTileValue(float xPos, float yPos, int[][] lvlData) {
+		int xCord = (int) (xPos / Game.TILES_SIZE);
+		int yCord = (int) (yPos / Game.TILES_SIZE);
+		return lvlData[yCord][xCord];
+	}
+
+	public static boolean IsTileSolid(int xTile, int yTile, int[][] lvlData) {
+		int value = lvlData[yTile][xTile];
 		/*
 		 * Quy ước:
 		 * Gốc tọa độ O của window nằm ở góc trên cùng bên trái màn hình
@@ -46,99 +63,147 @@ public class HelpMethods {
 		 * thành ra, yIndex là chỉ số hàng, xIndex là chỉ số cột của ma trận lvlData[][]
 		 */
 
-		if (value >= 48 || value < 0 || value != 11)  return true;
+		switch (value) {
+		case 11, 48, 49:
+			return false;
+		default:
+			return true;
+		}
 		/*
 		 (value >= 48 || value < 0) -> nếu mình di chuyển ra ngoài biên, thì solid -> không đi đâu được nữa
 		 value != 11 -> nếu mình không đi vào ô không gian trống -> chạm vào vật thể -> solid
 		 */
 
-		return false;
+
 	}
+	
 
 	public static float GetEntityXPosNextToWall(Rectangle2D.Float hitbox, float xSpeed) {
 		// Ham nay se tra ve vi tri cua hitbox.x ke canh voi wall nhat co the
-
-		int currentTile = (int)(hitbox.x / Game.TILES_SIZE);
+		
+		int currentTile = (int) (hitbox.x / Game.TILES_SIZE);
 		// cot ma nhan vat dang dung
-
-		if(xSpeed > 0) {
+		
+		if (xSpeed > 0) {
 			// RIGHT
 			// Nhan vat dang di ve ben phai
-
-
-			int tileXPos = currentTile * Game.TILES_SIZE; // Vi tri cua nhan vat theo x cua tile hien tai
-			int xOffset = (int)(Game.TILES_SIZE - hitbox.width);
+			
+			int tileXPos = currentTile * Game.TILES_SIZE;
+			// Vi tri cua nhan vat theo x cua tile hien tai
+			
+			int xOffset = (int) (Game.TILES_SIZE - hitbox.width);
 			// Khoang cach giua canh ben trai cua hitbox voi canh ben trai cua currentTile
 			// khi hitbox da sat vao canh ben phai cua currentTile;
 			//  |        |
 			//  |_____|  |
 			//  | kc  |  |
+			
 			return tileXPos + xOffset - 1;
 			// Tru 1 de hitbox khong overlap voi wall
 			// tuc la luon ton tai khoang cach dx = 1 pixel giua bien hitbox voi wall
-		}else {
-			// LEFT
+			
+		} else {
+			// Left
 			// Nhan vat dang di ve ben trai
 			return currentTile * Game.TILES_SIZE;
 		}
-
 		//xSpeed != 0 vi neu xSpeed == 0 se khong co collision
 	}
 
 	public static float GetEntityYPosUnderRoofOrAboveFloor(Rectangle2D.Float hitbox, float airSpeed) {
 		//Ham nay tuong tu ham ben tren nhung doi voi y
-
-		int currentTile = (int)(hitbox.y / Game.TILES_SIZE);// hang ma nhan vat dang dung
-
-		if(airSpeed > 0) { // FALLING - TOUCHING FLOOR
-			int tileYPos = currentTile * Game.TILES_SIZE; // Vi tri cua nhan vat theo x cua tile hien tai
-			int yOffset = (int)(Game.TILES_SIZE - hitbox.height);
-			return  tileYPos + yOffset - 1;
-		}else { // JUMPING
+		int currentTile = (int) (hitbox.y / Game.TILES_SIZE);
+		if (airSpeed > 0) {
+			// Falling - touching floor
+			int tileYPos = currentTile * Game.TILES_SIZE;
+			int yOffset = (int) (Game.TILES_SIZE - hitbox.height);
+			return tileYPos + yOffset - 1;
+		} else
+			// Jumping
 			return currentTile * Game.TILES_SIZE;
-		}
+
 	}
 
-	public static boolean IsEntityOnFloor(Rectangle2D.Float hitbox, int[][] lvldata) {
+	public static boolean IsEntityOnFloor(Rectangle2D.Float hitbox, int[][] lvlData) {
 		// Check the pixel below bottom-left and bottom-right
-		if(!IsSolid(hitbox.x, hitbox.y + hitbox.height + 1, lvldata)) {// Kiem tra diem Duoi Trai cua hitbox
-			if(!IsSolid(hitbox.x + hitbox.width, hitbox.y + hitbox.height + 1, lvldata)) {// Kiem tra Duoi phai
+		if (!IsSolid(hitbox.x, hitbox.y + hitbox.height + 1, lvlData))
+			if (!IsSolid(hitbox.x + hitbox.width, hitbox.y + hitbox.height + 1, lvlData))
 				return false;
-			}
-		}
 		return true;
 	}
 
-	public static boolean IsFloor(Rectangle2D.Float hitbox, float xSpeed, int[][] lvldata) {
-		return IsSolid(hitbox.x + xSpeed, hitbox.y + hitbox.height + 1, lvldata);
+	public static boolean IsFloor(Rectangle2D.Float hitbox, float xSpeed, int[][] lvlData) {
 		// Check diem duoi phai
+		if (xSpeed > 0)
+			return IsSolid(hitbox.x + hitbox.width + xSpeed, hitbox.y + hitbox.height + 1, lvlData);
+		else
+			return IsSolid(hitbox.x + xSpeed, hitbox.y + hitbox.height + 1, lvlData);
 	}
 
-	public static boolean IsAllTileWalkable(int xStart, int xEnd, int y, int[][] lvldata) {
-		// Kiem tra xem tu xStart den xEnd co di duoc khong
-		// Hay noi cach khac la co vat can o giua 2 diem khong
-		for(int i = 0; i < xEnd - xStart; i++) {
-			if (IsTileSolid(xStart + i, y, lvldata)) {
+	public static boolean IsFloor(Rectangle2D.Float hitbox, int[][] lvlData) {
+		if (!IsSolid(hitbox.x + hitbox.width, hitbox.y + hitbox.height + 1, lvlData))
+			if (!IsSolid(hitbox.x, hitbox.y + hitbox.height + 1, lvlData))
 				return false;
-			}
-
-			// Truong hop ma co mot ho o giua player va enemy
-			if (!IsTileSolid(xStart + i, y + 1, lvldata)) {
-				return false;
-			}
-		}
 		return true;
 	}
 
-	/// kiem tra xem giua 2 object co vat the khong
-	public static boolean IsSightClear(int[][] lvldata, Rectangle2D.Float firsthitbox, Rectangle2D.Float secondhitbox, int yTile) {
-		int firstXTile = (int) (firsthitbox.x / Game.TILES_SIZE);
-		int secondXTile = (int) (secondhitbox.x / Game.TILES_SIZE);
+	public static boolean CanCannonSeePlayer(int[][] lvlData, Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
+		int firstXTile = (int) (firstHitbox.x / Game.TILES_SIZE);
+		int secondXTile = (int) (secondHitbox.x / Game.TILES_SIZE);
 
-		if(firstXTile > secondXTile) {
-			return IsAllTileWalkable(secondXTile, firstXTile, yTile, lvldata);
-		}
-		return IsAllTileWalkable(firstXTile, secondXTile, yTile, lvldata);
-    }
+		if (firstXTile > secondXTile)
+			return IsAllTilesClear(secondXTile, firstXTile, yTile, lvlData);
+		else
+			return IsAllTilesClear(firstXTile, secondXTile, yTile, lvlData);
+	}
 
+	public static boolean IsAllTilesClear(int xStart, int xEnd, int y, int[][] lvlData) {
+		for (int i = 0; i < xEnd - xStart; i++)
+			if (IsTileSolid(xStart + i, y, lvlData))
+				return false;
+		return true;
+	}
+
+	public static boolean IsAllTilesWalkable(int xStart, int xEnd, int y, int[][] lvlData) {
+		if (IsAllTilesClear(xStart, xEnd, y, lvlData))
+			for (int i = 0; i < xEnd - xStart; i++) {
+				
+				// Truong hop ma co mot ho o giua player va enemy
+				if (!IsTileSolid(xStart + i, y + 1, lvlData))
+					return false;
+			}
+		return true;
+	}
+
+	// Player can sometimes be on an edge and in sight of enemy.
+	// The old method would return false because the player x is not on edge.
+	// This method checks both player x and player x + width.
+	// If tile under playerBox.x is not solid, we switch to playerBox.x +
+	// playerBox.width;
+	// One of them will be true, because of prior checks.
+
+	public static boolean IsSightClear(int[][] lvlData, Rectangle2D.Float enemyBox, Rectangle2D.Float playerBox, int yTile) {
+		int firstXTile = (int) (enemyBox.x / Game.TILES_SIZE);
+
+		int secondXTile;
+		if (IsSolid(playerBox.x, playerBox.y + playerBox.height + 1, lvlData))
+			secondXTile = (int) (playerBox.x / Game.TILES_SIZE);
+		else
+			secondXTile = (int) ((playerBox.x + playerBox.width) / Game.TILES_SIZE);
+
+		if (firstXTile > secondXTile)
+			return IsAllTilesWalkable(secondXTile, firstXTile, yTile, lvlData);
+		else
+			return IsAllTilesWalkable(firstXTile, secondXTile, yTile, lvlData);
+	}
+
+	public static boolean IsSightClear_OLD(int[][] lvlData, Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
+		int firstXTile = (int) (firstHitbox.x / Game.TILES_SIZE);
+		int secondXTile = (int) (secondHitbox.x / Game.TILES_SIZE);
+
+		if (firstXTile > secondXTile)
+			return IsAllTilesWalkable(secondXTile, firstXTile, yTile, lvlData);
+		else
+			return IsAllTilesWalkable(firstXTile, secondXTile, yTile, lvlData);
+	}
 }
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,27 +1,66 @@
 package utilz;
 
-import java.awt.Color;
+import entities.PlayerCharacter;
+
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import static utilz.Constants.EnemyConstants.CRABBY;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
-import entities.Crabby;
-import main.Game;
-
 public class LoadSave {
+    public static final String PLAYER_PIRATE = "player_sprites.png";
+    public static final String PLAYER_ORC = "player_orc.png";
+    public static final String PLAYER_SOLDIER = "player_soldier.png";
+    public static final String LEVEL_ATLAS = "outside_sprites.png";
+    public static final String MENU_BUTTONS = "button_atlas.png";
+    public static final String MENU_BACKGROUND = "menu_background.png";
+    public static final String PAUSE_BACKGROUND = "pause_menu.png";
+    public static final String SOUND_BUTTONS = "sound_button.png";
+    public static final String URM_BUTTONS = "urm_buttons.png";
+    public static final String VOLUME_BUTTONS = "volume_buttons.png";
+    public static final String MENU_BACKGROUND_IMG = "background_menu.png";
+    public static final String PLAYING_BG_IMG = "playing_bg_img.png";
+    public static final String BIG_CLOUDS = "big_clouds.png";
+    public static final String SMALL_CLOUDS = "small_clouds.png";
+    public static final String CRABBY_SPRITE = "crabby_sprite.png";
+    public static final String STATUS_BAR = "health_power_bar.png";
+    public static final String COMPLETED_IMG = "completed_sprite.png";
+    public static final String POTION_ATLAS = "potions_sprites.png";
+    public static final String CONTAINER_ATLAS = "objects_sprites.png";
+    public static final String TRAP_ATLAS = "trap_atlas.png";
+    public static final String CANNON_ATLAS = "cannon_atlas.png";
+    public static final String CANNON_BALL = "ball.png";
+    public static final String DEATH_SCREEN = "death_screen.png";
+    public static final String OPTIONS_MENU = "options_background.png";
+    public static final String PINKSTAR_ATLAS = "pinkstar_atlas.png";
+    public static final String QUESTION_ATLAS = "question_atlas.png";
+    public static final String EXCLAMATION_ATLAS = "exclamation_atlas.png";
+    public static final String SHARK_ATLAS = "shark_atlas.png";
+    public static final String CREDITS = "credits_list.png";
+    public static final String GRASS_ATLAS = "grass_atlas.png";
+    public static final String TREE_ONE_ATLAS = "tree_one_atlas.png";
+    public static final String TREE_TWO_ATLAS = "tree_two_atlas.png";
+    public static final String GAME_COMPLETED = "game_completed.png";
+    public static final String RAIN_PARTICLE = "rain_particle.png";
+    public static final String WATER_TOP = "water_atlas_animation.png";
+    public static final String WATER_BOTTOM = "water.png";
+    public static final String SHIP = "ship.png";
 
-	public static final String PLAYER_ATLAS = "player_sprites.png";
-	public static final String LEVEL_ATLAS = "outside_sprites.png";
-	public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
-	public static final String PLAYING_BG_IMG = "playing_bg_img.png";
-	public static final String BIG_CLOUDS = "big_clouds.png";
-	public static final String SMALL_CLOUDS = "small_clouds.png";
-	public static final String CRABBY_SPRITE = "crabby_sprite.png";
-	public static final String STATUS_BAR = "health_power_bar.png";
+
+    public static BufferedImage[][] loadAnimations(PlayerCharacter pc) {
+        BufferedImage img = LoadSave.GetSpriteAtlas(pc.playerAtlas);
+        BufferedImage[][] animations = new BufferedImage[pc.rowA][pc.colA];
+        for (int j = 0; j < animations.length; j++)
+            for (int i = 0; i < animations[j].length; i++)
+                animations[j][i] = img.getSubimage(i * pc.spriteW, j * pc.spriteH, pc.spriteW, pc.spriteH);
+
+        return animations;
+    }
+
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
@@ -41,39 +80,38 @@ public class LoadSave {
 		return img;
 	}
 
-	public static ArrayList<Crabby> GetCrabs() {
-		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
-		ArrayList<Crabby> list = new ArrayList<>();
+    public static BufferedImage[] GetAllLevels() {
+        URL url = LoadSave.class.getResource("/lvls");
+        File file = null;
 
-		for (int j = 0; j < img.getHeight(); j++) {
-			for (int i = 0; i < img.getWidth(); i++) {
-				Color color = new Color(img.getRGB(i, j));
-				int value = color.getGreen();
-				if (value == CRABBY)
-					list.add(new Crabby(i * Game.TILES_SIZE, j * Game.TILES_SIZE - Game.TILES_SIZE));
-			}
-		}
+        try {
+            file = new File(url.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 
-		return list;
+        File[] files = file.listFiles();
+        File[] filesSorted = new File[files.length];
 
-	}
-	
-	public static int[][] GetLevelData(){
+        for (int i = 0; i < filesSorted.length; i++)
+            for (int j = 0; j < files.length; j++) {
+                if (files[j].getName().equals((i + 1) + ".png"))
+                    filesSorted[i] = files[j];
 
-		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
-		int[][] lvlData = new int[img.getHeight()][img.getWidth()];
+            }
 
-		
-		for (int j = 0; j < img.getHeight(); j++) {
-			for (int i = 0; i < img.getWidth(); i++) {
-				Color color = new Color(img.getRGB(i, j));
-				int value = color.getRed();
-				if (value >= 48) value = 0; //có thể ảnh bitmap của mình bị thiết kế với kích cỡ lớn hơn 12*4
-				lvlData[j][i] = value;
-			}
-		}
-		return lvlData;
-	}
+        BufferedImage[] imgs = new BufferedImage[filesSorted.length];
+
+        for (int i = 0; i < imgs.length; i++)
+            try {
+                imgs[i] = ImageIO.read(filesSorted[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        return imgs;
+    }
+    
 	/*
 	Trên thực tế, người ta sẽ phải import map gồm các hoạt ảnh của outside từ outside.png, 
 	tạo một mảng để lưu các subimage theo kích cỡ 32*32 từ map đó đưa vào lvlSprite[] trong lvlManager với 
@@ -91,4 +129,5 @@ public class LoadSave {
 	
 	ảnh này chính là lvl_one_data.png có kích cỡ 12*4 pixel !
 	 */
+
 }
