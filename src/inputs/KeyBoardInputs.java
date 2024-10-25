565aabda75dd6d4459a2a92bf165ffd2c5b5346c
ppap-1264589 <doxuanhoangnguyenthienthuat@gmail.com>
2024-10-25 22:52:23 +0700

    Update inputs
    Bổ sung các thao tác tương ứng của keyboard và mouse cho các gamestate khác nhau

--- src/inputs/KeyBoardInputs.java
+++ src/inputs/KeyBoardInputs.java
@@ -19,27 +19,28 @@ public class KeyBoardInputs implements KeyListener{
 
 	} 
 	
-	@Override
-	public void keyReleased(KeyEvent e) {
-		switch (Gamestate.state) {
-			case MENU:
-				gamePanel.getGame().getMenu().keyReleased(e);
-				break;
-			case PLAYING:
-				gamePanel.getGame().getPlaying().keyReleased(e);
-				break;
-		}
-	}
-	
-	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (Gamestate.state) {
-			case MENU:
-				gamePanel.getGame().getMenu().keyPressed(e);
-				break;
-			case PLAYING:
-				gamePanel.getGame().getPlaying().keyPressed(e);
-				break;
-		}
-	}
+	/*Tùy theo sự kiện gamestate hiện tại, mà việc keyRelease sẽ phụ thuộc
+	 * chả hạn, đang chơi, thì keyRelease sẽ làm nhân vật ngừng di chuyển
+	 * trong khi ở credit lại làm hình ảnh chạy nhanh lên
+	 */
+	@SuppressWarnings("incomplete-switch")
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().keyReleased(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().keyReleased(e);
+            case CREDITS -> gamePanel.getGame().getCredits().keyReleased(e);
+        }
+    }
+
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().keyPressed(e);
+            case PLAYER_SELECTION -> gamePanel.getGame().getPlayerSelection().keyPressed(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().keyPressed(e);
+            case OPTIONS -> gamePanel.getGame().getGameOptions().keyPressed(e);
+        }
+    }
 }
--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -8,63 +8,71 @@ import gamestates.Gamestate;
 import main.GamePanel;
 
 public class MouseInputs implements MouseListener, MouseMotionListener{
+	
 	private GamePanel gamePanel;
-	public MouseInputs (GamePanel gamePanel) {
+	
+	public MouseInputs(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 	}
 	
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		
-	}
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        switch (Gamestate.state) {
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseDragged(e);
+            
+            /*Nếu đang ở gamestate Options, update theo hàm mouseDragged, biến MouseEvent e
+             * Ví dụ ở đây, ta update mouseDragged ở GameOptions, tương đương với việc
+             * update mouseDragged ở audioOptions
+             * mouseDragged ở audioOptions sẽ được cài đặt cụ thể
+             * 
+             * chú ý là mouseDragged là sự kiện chỉ có ở trong gamestate Options và 
+             * các hành động cụ thể liên quan đến sự kiện mouseDragged chỉ liên quan đến VolumeButton
+             */
+            case OPTIONS -> gamePanel.getGame().getGameOptions().mouseDragged(e);
+        }
+    }
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		switch (Gamestate.state) {
-		case MENU:
-			gamePanel.getGame().getMenu().mouseMoved(e);
-			break;
-		case PLAYING:
-			gamePanel.getGame().getPlaying().mouseMoved(e);
-			break;
-		}
-	}
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().mouseMoved(e);
+            case PLAYER_SELECTION -> gamePanel.getGame().getPlayerSelection().mouseMoved(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseMoved(e);
+            case OPTIONS -> gamePanel.getGame().getGameOptions().mouseMoved(e);
+        }
+    }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		switch (Gamestate.state) {
-		case MENU:
-			gamePanel.getGame().getMenu().mouseClicked(e);
-			break;
-		case PLAYING:
-			gamePanel.getGame().getPlaying().mouseClicked(e);
-			break;
-		}
-	}
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        switch (Gamestate.state) {
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseClicked(e);
+        }
+    }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		switch (Gamestate.state) {
-		case MENU:
-			gamePanel.getGame().getMenu().mousePressed(e);
-			break;
-		case PLAYING:
-			gamePanel.getGame().getPlaying().mousePressed(e);
-			break;
-		}
-	}
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public void mousePressed(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().mousePressed(e);
+            case PLAYER_SELECTION -> gamePanel.getGame().getPlayerSelection().mousePressed(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().mousePressed(e);
+            case OPTIONS -> gamePanel.getGame().getGameOptions().mousePressed(e);
+        }
+    }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		switch (Gamestate.state) {
-		case MENU:
-			gamePanel.getGame().getMenu().mouseReleased(e);
-			break;
-		case PLAYING:
-			gamePanel.getGame().getPlaying().mouseReleased(e);
-			break;
-		}
-	}
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().mouseReleased(e);
+            case PLAYER_SELECTION -> gamePanel.getGame().getPlayerSelection().mouseReleased(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseReleased(e);
+            case OPTIONS -> gamePanel.getGame().getGameOptions().mouseReleased(e);
+        }
+    }
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
