3fe0a0c2d0d98bd54fd648df6e399c3f005f56e8
ppap-1264589 <doxuanhoangnguyenthienthuat@gmail.com>
2024-10-25 09:55:41 +0700

    Update các file chưa có comment

--- src/gamestates/Menu.java
+++ src/gamestates/Menu.java
@@ -1,31 +1,34 @@
 package gamestates;
 
-import main.Game;
-import ui.MenuButton;
-import utilz.LoadSave;
-
-import java.awt.*;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
+import main.Game;
+import ui.MenuButton;
+import utilz.LoadSave;
 
-public class Menu extends State implements Statemethods{
-	
-	private MenuButton[] buttons = new MenuButton[3];
-	private BufferedImage backgroundImg;
-	private int menuX, menuY, menuWidth, menuHeight;
-	
+public class Menu extends State implements Statemethods {
+
+    private MenuButton[] buttons = new MenuButton[4];
+    private BufferedImage backgroundImg, backgroundImgPink;
+    private int menuX, menuY, menuWidth, menuHeight;
+    /*
+     * Vị trí góc trên bên trái (menuX, menuY) để vẽ menu có độ rộng menuWidth, menuHeight
+     */
+    
     /*
      * Khi khởi tạo Menu, ngoài việc khởi tạo đối tượng menu và playing qua Game.java
      * còn phải load các trạng thái của Button trên menu nữa
      */
-	public Menu(Game game) {
+    public Menu(Game game) {
         super(game);
         loadButtons();
         loadBackground();
+        backgroundImgPink = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND_IMG);
     }
-	
+
 	/*
 	 * Điểm (menuX, menuY) của MENU_BACKGROUND phải nằm ở vị trí sao cho
 	 * cả ảnh menu phải nằm ở chính giữa màn hình
@@ -33,108 +36,109 @@ public class Menu extends State implements Statemethods{
 	 * 
 	 * Vị trí thích hợp cho menuY là khoảng 25 pixel tính từ phần biên trên cùng
 	 */
-	private void loadBackground() {
-		backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND);
-		menuWidth = (int)(backgroundImg.getWidth() * Game.SCALE);
-		menuHeight = (int)(backgroundImg.getHeight() * Game.SCALE);
-		menuX = Game.GAME_WIDTH/2 - menuWidth/2;
-		menuY = (int)(25 * Game.SCALE);
-	}
-	/**
-	 * nên tạo một lớp mới để giải thích công thức loadBackground này rõ ràng hơn
-	 **/
-
+    private void loadBackground() {
+        backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND);
+        menuWidth = (int) (backgroundImg.getWidth() * Game.SCALE);
+        menuHeight = (int) (backgroundImg.getHeight() * Game.SCALE);
+        menuX = Game.GAME_WIDTH / 2 - menuWidth / 2;
+        menuY = (int) (25 * Game.SCALE);
+    }
 
 	/* Tham số thứ 2: Đại diện cho vị trí cần in ra theo tọa độ y của nút trên menu
 	 * Tham số thứ 3: 0 ứng với gamestate = playing 
 	 */
     private void loadButtons() {
-		buttons[0] = new MenuButton(Game.GAME_WIDTH/2, (int)(150 * Game.SCALE), 0, Gamestate.PLAYING);
-		buttons[1] = new MenuButton(Game.GAME_WIDTH/2, (int)(220 * Game.SCALE), 1, Gamestate.OPTIONS);
-		buttons[2] = new MenuButton(Game.GAME_WIDTH/2, (int)(290 * Game.SCALE), 2, Gamestate.QUIT);
-	}
+        buttons[0] = new MenuButton(Game.GAME_WIDTH / 2, (int) (130 * Game.SCALE), 0, Gamestate.PLAYER_SELECTION);
+        buttons[1] = new MenuButton(Game.GAME_WIDTH / 2, (int) (200 * Game.SCALE), 1, Gamestate.OPTIONS);
+        buttons[2] = new MenuButton(Game.GAME_WIDTH / 2, (int) (270 * Game.SCALE), 3, Gamestate.CREDITS);
+        buttons[3] = new MenuButton(Game.GAME_WIDTH / 2, (int) (340 * Game.SCALE), 2, Gamestate.QUIT);
+    }
 
-
+    
     /* Trong mỗi lần update hệ thống, duyệt qua toàn bộ các nút hiện đang có trên menu
-     * và update chúng 
+     * và update, render chúng
      */
-	@Override
+    @Override
     public void update() {
-        for (MenuButton mb: buttons) {
-        	mb.update();
-        }
+        for (MenuButton mb : buttons)
+            mb.update();
     }
 
     @Override
     public void draw(Graphics g) {
-    	g.drawImage(backgroundImg, menuX, menuY, menuWidth, menuHeight, null);
-        for (MenuButton mb: buttons) {
-        	mb.draw(g);
-        }
+        g.drawImage(backgroundImgPink, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
+        g.drawImage(backgroundImg, menuX, menuY, menuWidth, menuHeight, null);
 
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    	
+        for (MenuButton mb : buttons)
+            mb.draw(g);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        for (MenuButton mb: buttons) {
-        	if (isIn(e, mb)) {
-        		mb.setMousePressed(true);
-        	}
+        for (MenuButton mb : buttons) {
+            if (isIn(e, mb)) {
+                mb.setMousePressed(true);
+            }
         }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        for (MenuButton mb: buttons) {
-        	if (isIn(e, mb)) {
-        		if(mb.isMousePressed()) {
-        			mb.applyGamestate();
-        		}
-        	}
-        	/*
-        	 * Giả sử mình click vào nút xong lại kéo chuột ra bên ngoài nút
-        	 * Thì hệ thống sẽ không tính là mình "đã click"
-        	 * Chỉ coi là bấm nhầm và không thực hiện thêm thao tác
-        	 */
+        for (MenuButton mb : buttons) {
+            if (isIn(e, mb)) {
+                if (mb.isMousePressed())
+                    mb.applyGamestate();
+            	/*
+            	 * Giả sử mình click vào nút xong lại kéo chuột ra bên ngoài nút
+            	 * Thì hệ thống sẽ không tính là mình "đã click"
+            	 * Chỉ coi là bấm nhầm và không thực hiện thêm thao tác
+            	 */
+                if (mb.getState() == Gamestate.PLAYING)
+                    game.getAudioPlayer().setLevelSong(game.getPlaying().getLevelManager().getLevelIndex());
+                break;
+
+            }
         }
         resetButtons(); //Release xong thì phải trả lại trạng thái cho nút
     }
 
     private void resetButtons() {
-    	for (MenuButton mb: buttons) {
-    		mb.resetBools();
-    	}
-	}
+        for (MenuButton mb : buttons)
+            mb.resetBools();
+    }
 
     /*
      * Nếu bắt được sự kiện chuột di chuyển, check xem:
      * Nếu chuột ở trong một nút B nào đó thì nút B chuyển trạng thái mouseOver = true
      * Nếu cũng là di chuột mà chuột lại không ở trong nút nào cả thì các nút vẫn mouseOver = false
      */
-	@Override
+    @Override
     public void mouseMoved(MouseEvent e) {
-       	for (MenuButton mb: buttons) {
-    		if (!isIn(e, mb)) {
-    			mb.setMouseOver(false);
-    		}
-    		else mb.setMouseOver(true);
-    	}
+        for (MenuButton mb : buttons)
+            mb.setMouseOver(false);
+
+        for (MenuButton mb : buttons)
+            if (isIn(e, mb)) {
+                mb.setMouseOver(true);
+                break;
+            }
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-//        if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-//            Gamestate.state = Gamestate.PLAYING;
-//        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
 
     }
+
 }
--- src/gamestates/State.java
+++ src/gamestates/State.java
@@ -2,6 +2,7 @@ package gamestates;
 
 import java.awt.event.MouseEvent;
 
+import audio.AudioPlayer;
 import main.Game;
 import ui.MenuButton;
 
@@ -12,7 +13,6 @@ public class State {
         this.game = game;
     }
     
-    
     // Xét xem một điểm (e.getX(), e.getY()) có nằm trong Rectangle mb.getBounds hay không?
     public boolean isIn(MouseEvent e, MenuButton mb){
     	return mb.getBounds().contains(e.getX(), e.getY());
@@ -22,5 +22,12 @@ public class State {
         return game;
     }
     
-    
+	@SuppressWarnings("incomplete-switch")
+	public void setGamestate(Gamestate state) {
+		switch (state) {
+			case MENU -> game.getAudioPlayer().playSong(AudioPlayer.MENU_1);
+			case PLAYING -> game.getAudioPlayer().setLevelSong(game.getPlaying().getLevelManager().getLevelIndex());
+		}
+		Gamestate.state = state;
+	}
 }
